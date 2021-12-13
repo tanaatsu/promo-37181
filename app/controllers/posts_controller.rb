6ac6f6e98c59f5119fb4eb089be7a6f9213fba24
tanaatsu <atsushi19890825@gmail.com>
2021-12-13 11:35:07 +0900

    商品詳細機能（編集、削除）実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except:[:index,:show]
 
   def index
     @posts = Post.order("created_at DESC").includes(:user)
@@ -20,6 +21,28 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
   
   private
