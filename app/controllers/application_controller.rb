2575562a0c47d73a588b705bbae934b752f7f5ec
tanaatsu <atsushi19890825@gmail.com>
2021-12-17 12:29:29 +0900

    マイページの編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:email,:password,:last_name,:first_name,:telephone,:birthday,
                                                      :industry_id,:reason_id,:person_id,:prefecture_id,:business_id,:store_name,:area,:opening_hours,:store_appeal,:image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email,:password,:last_name,:first_name,:telephone,:birthday,
+                                                      :industry_id,:reason_id,:person_id,:prefecture_id,:business_id,:store_name,:area,:opening_hours,:store_appeal,:image])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   'users#show'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+    }
   root to: "posts#index"
   resources :posts, only:[:index,:new,:create,:show,:edit,:update,:destroy] do
     resources :records,only:[:index,:create]
     resources :comments,only: [:create]
   end
-  resources :users ,only: [:show,:edit,:update]
+  resources :users ,only: [:show]
 end
