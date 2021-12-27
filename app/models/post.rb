19493c619a3fa37cd0f047a5abd0e84ff9d22667
tanaatsu <atsushi19890825@gmail.com>
2021-12-27 23:37:55 +0900

    削除機能のダイアログ表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_one :record
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
   with_options presence: true do
